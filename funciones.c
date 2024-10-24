cdd9e325238ab528e2f1dfabfe21b288f346b378
LauVega17 <vegalautaro1763@gmail.com>
2024-10-24 18:22:59 -0300

    Revert "Merge branch 'luca' into lau"
    This reverts commit 5a2e6c5e3a7ff292a5ce9d8ce3302221e1ef53ab, reversing
    changes made to 690d26131eec9d8035ee1627d2649aaf1aab2907.

--- funciones.c
+++ funciones.c
@@ -1,7 +1,5 @@
 #include "funciones.h"
 
-/** Funciones CURL **/
-
 size_t WriteCallback(char* contents, size_t tamanyo, size_t nmemb, void *userp)
 {
     size_t totalSize = tamanyo * nmemb;
@@ -45,8 +43,6 @@ int ejecutarSolicitud(CURL* curl)
     }
     return TODO_OK;
 }
-
-/** Lectura y muestra de la info del jugador **/
 
 void mostrarJugador(const void* dato)
 {
@@ -94,38 +90,7 @@ int ingresoDeNombresAListaSimple(t_lista* listaDeJugadores, unsigned* cantidadDe
     return OK;
 }
 
-void detallesJuego(tConfig* config)
-{
-    char op;
-    tConfig* elemento;
-
-    printf("Niveles de dificultad.\n F - Facil\n M - Medio\n D - Dificil\n");
-
-    do
-    {
-        printf("\nIngrese el nivel de dificultad: ");
-        scanf("%c", &op);
-        getchar();
-
-        if (op != 'F' && op != 'M' && op != 'D') {
-            printf("\nOpcion incorrecta\n");
-        }
-    }while(op != 'F' && op != 'M' && op != 'D');
-
-    elemento = (tConfig*)(buscarDificultad(config, &op, sizeof(tConfig), cmpDificultad));
-
-    printf("\n-------------------------------------------\n");
-    printf("Configuracion de la partida: \n");
-    printf("Nivel: %c\n", elemento->nivel);
-    printf("Tiempo que se muestra la secuencia: %i\n", elemento->tiempoSecuenciaEnPantalla);
-    printf("Tiempo para contestar: %u\n", elemento->tiempoContestar);
-    printf("Cantidad de vidas: %u\n", elemento->cantVidas);
-    printf("-------------------------------------------\n");
-    system("pause");
-    system("cls");
-}
-
-void juego(void* recursos, tConfig* config)
+void juego(void* recursos)
 {
     if(!ingresoDeNombresAListaSimple(&(((tRecursosMenu*)recursos)->listaDeJugadores), &(((tRecursosMenu*)recursos)->cantidadDeJugadores)))
     {
@@ -133,12 +98,21 @@ void juego(void* recursos, tConfig* config)
     }
     else
     {
-        detallesJuego(config);
         mezclarListaSimpleAleatoriamente(&(((tRecursosMenu*)recursos)->listaDeJugadores), ((tRecursosMenu*)recursos)->cantidadDeJugadores);
-        printf("\nLista de jugadores:\n");
+        printf("Lista de jugadores:\n");
         mostrarListaSimpleEnOrden(&(((tRecursosMenu*)recursos)->listaDeJugadores), mostrarJugador);
         system("pause");
         system("cls");
+    }
+}
+
+void switchTextoMenu(int opcion, void* recursosMenu)
+{
+    switch(opcion)
+    {
+    case 'A':
+        juego(recursosMenu);
+        break;
     }
 }
 
@@ -173,11 +147,6 @@ void crearLoteDePrueba (const char* nombreArch)
 int cmpConfigNivel (const void* a, const void* b)
 {
     return ((tConfig*)a)->nivel - ((tConfig*)b)->nivel;
-}
-
-int cmpDificultad(const void* a, const void* b)
-{
-    return ((tConfig*)a)->nivel - *((char*)b);
 }
 
 int insertarAlFinalVector (void* v, const void* d, int ce, size_t tam)
@@ -226,20 +195,6 @@ int leerArchivoConfig (const char* nombreArch, tConfig* vecConfig)
     return TODO_OK;
 }
 
-void* buscarDificultad(void* v, const void* dato, size_t tam, int(*cmp)(const void* a, const void* b))
-{
-    int i;
-    void* elemento = v;
-
-    for(i=0; i<TAM_VECTOR_CONFIG; i++)
-    {
-        if(cmp(elemento, dato) == 0)
-            return elemento;
-        elemento+=tam;
-    }
-}
-
-
 int buscarPorClaveVector (void* v, const void* d, int ce, size_t tam, int (*cmp)(const void* x, const void* y))
 {
     void* i = v;
@@ -279,50 +234,4 @@ void trozarArchivoVariable (char* s, tConfig* d)
 
     /** Nivel */
     sscanf(s, "%c", &d->nivel);
-}
-
-void cargarOpcion(char* op)
-{
-    do
-    {
-        printf("\nIngrese una opcion: ");
-        scanf("%c", op);
-        getchar();
-
-        if (*op != 'A' && *op != 'B') {
-            printf("\nOpcion incorrecta\n");
-        }
-    }while(*op != 'A' && *op != 'B');
-}
-
-
-void switchMenu(char opcion, void* recursos, tConfig* config)
-{
-    switch(opcion)
-    {
-    case 'A':
-        juego(recursos, config);
-        break;
-    case 'B':
-        system("cls");
-        printf("Saliendo...\n");
-        exit(1);
-    }
-}
-
-void menuNuevo(char textoMenu[][MAX_TAM_TEXTO], unsigned cantidadDeRegistros, void* recursosMenu, tConfig* config)
-{
-    int i;
-    char op;
-
-    for(i=0; i<cantidadDeRegistros; i++)
-    {
-        printf("%s\n", *textoMenu);
-        textoMenu++;
-    }
-
-    cargarOpcion(&op);
-
-    switchMenu(op, recursosMenu, config);
-
 }
