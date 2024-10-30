acb127e71d3e84e6cf1c35ecb976db0febc2e502
ezequielmp <ezequielmunozpalazzo@gmail.com>
2024-10-30 20:53:09 -0300

    Cambio de argumentos a Main por Macro

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "funciones.h"
 #include "menu.h"
 #include <unistd.h>
+#define ARCHIVO_CONFIG "config.txt"
 
-int main(int argc, char* argv[])
+int main()
 {
     tRecursosMenu recursos;
     tConfig vectorConfig[TAM_VECTOR_CONFIG];
@@ -18,14 +19,14 @@ int main(int argc, char* argv[])
     recursos.colores[3] = 'V';
     unsigned cantidadDeRegistros = sizeof(textoMenuPrincipal) / MAX_TAM_TEXTO;
 
-    if(access(*(argv + NOMBRE_ARCH_CONFIG), F_OK) != 0)
-        crearLoteDePrueba(*(argv + NOMBRE_ARCH_CONFIG));
+    if(access(ARCHIVO_CONFIG, F_OK) != 0)
+        crearLoteDePrueba(ARCHIVO_CONFIG);
 
     //LEER ARCHIVO CONFIGURACIONES
 
     crearListaSimple(&recursos.listaDeJugadores);
 
-    if(!leerArchivoConfig(*(argv + NOMBRE_ARCH_CONFIG), vectorConfig))
+    if(!leerArchivoConfig(ARCHIVO_CONFIG, vectorConfig))
     {
         printf("El archivo de configuracion no contiene un formato valido. Revise el archivo README para mas informacion.");
         return ERR_ARCH_CONFIG;
